8589e44c95040b7abbcd60ad2caf3b2194c24dae
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-09 10:35:26 -0500

    Initial Commit

---
+++ Hasty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hasty
+//
+//  Created by Vidur Khanal on 09/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hasty/HastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HastyApp.swift
+//  Hasty
+//
+//  Created by Vidur Khanal on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
