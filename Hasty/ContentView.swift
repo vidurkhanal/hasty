5cbcf24cd4f920eeb98fb8f0d02a26a31a76c60b
Vidur Khanal <vkhanal@caldwell.edu>
2021-02-16 22:05:11 -0500

    BULT BASIC UI FOR THE CARD GAME

--- Hasty/ContentView.swift
+++ Hasty/ContentView.swift
@@ -9,8 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        ZStack {
+            Image("background")
+                .scaledToFit()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    
+            VStack {
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200.0, height: 200.0)
+                        
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Image("back")
+                    Spacer()
+                    Image(/*@START_MENU_TOKEN@*/"back"/*@END_MENU_TOKEN@*/)
+
+                }
+                .padding(.horizontal, 50.0)
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                
+                
+                HStack(alignment: .center) {
+                    VStack {
+                        Text("Player 1")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                        Text("0")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text(/*@START_MENU_TOKEN@*/"Player 2"/*@END_MENU_TOKEN@*/)
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                        Text(/*@START_MENU_TOKEN@*/"0"/*@END_MENU_TOKEN@*/)
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                }
+                .padding(.horizontal, 70.0)
+                
+                Spacer()
+
+            }
+
+        }
+           
     }
 }
 
